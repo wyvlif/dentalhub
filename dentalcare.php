baf57e98ba91a5f5dfc96862506c0452862f26b8
Wiclif Hirwa <151554842+wyvlif@users.noreply.github.com>
2025-03-19 08:16:03 +0200

    Add files via upload

---
+++ dentalcare.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="I-E=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A Complete Dentist Website design </title>
+
+    <!-- fontawesome cdn link -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    
+    <!-- bootstrap cdn link -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+
+    <!-- Custom css file link -->
+     <link rel="stylesheet" href="dentalcare.css">
+
+</head>
+<body>
+    <!-- header section start -->
+    <header class="header fixed-top">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+
+                <a href="#home" class="logo">Dental <span>Care.</span></a>
+
+                <nav class="nav">
+                    <a href="#home">home</a>
+                    <a href="#about">about</a>
+                    <a href="#services">services</a>
+                    <a href="#reviews">reviews</a>
+                    <a href="#contact">contact</a>
+                </nav>
+
+                <a href="#contact" class="link-btn">Make appointment</a>
+
+                <div id="menu-btn" class="fa fa-bars"></div>
+
+            </div>
+        </div>
+
+    </header>
+    <!-- header section Exits -->
+    <!-- home section starts -->
+     <section class="home" id="home">
+        <div class="container">
+            <div class="row min-vh-100 align-items-center">
+                <div class="content text-center text-md-left">
+                    <h3>Let us brighten your smile</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatum velit inventore quibusdam sit, dignissimos pariatur cum commodi voluptate mollitia, illum cupiditate? Qui autem cupiditate veritatis error inventore fugiat distinctio.</p>
+                    <a href="#contact" class="link-btn">Make appointment</a>
+                </div>
+            </div>
+        </div>
+     </section>
+    <!-- home section Exits -->
+    <!-- about section starts -->
+
+    <section class="about" id="about">
+        <div class="container">
+            <div class="row align-items-center">
+
+                <div class="col-md-6 image">
+                    <img src="image 9.jpg" class="w-100 mb-4 mb-md-0" alt="">
+                </div>
+
+                <div class="col-md-6 content">
+                    <span>about us</span>
+                    <h3>True health care for your family</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta repudiandae nisi, placeat sunt nulla dolores similique, velit excepturi dolorum libero iure omnis accusantium vero illo. Neque, quis vero. Inventore, architecto?
+                    </p>
+                    <a href="#contact" class="link-btn">make appointment</a>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- about section Ends -->
+    <!-- services section starts -->
+
+    <section class="services" id="services">
+        <h1 class="heading">Our Services</h1>
+
+        <div class="box-container container">
+            <div class="box">
+                <img src="image 4.png" alt="">
+                <h3>Alignment specialist</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sint quasi minus officia repellat fugit neque qui recusandae debitis saepe. Dignissimos ex fugiat vitae odio laudantium cum dolore. Nihil, veritatis.</p>
+            </div>
+
+            <div class="box">
+                <img src="image 4.png" alt="">
+                <h3>Cosmetic dentistry</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sint quasi minus officia repellat fugit neque qui recusandae debitis saepe. Dignissimos ex fugiat vitae odio laudantium cum dolore. Nihil, veritatis.</p>
+            </div>
+
+            <div class="box">
+                <img src="image 1.png" alt="">
+                <h3>Oral hygiene Expert</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sint quasi minus officia repellat fugit neque qui recusandae debitis saepe. Dignissimos ex fugiat vitae odio laudantium cum dolore. Nihil, veritatis.</p>
+            </div>
+
+            <div class="box">
+                <img src="root-canal.png" alt="">
+                <h3>Root canal specialist</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sint quasi minus officia repellat fugit neque qui recusandae debitis saepe. Dignissimos ex fugiat vitae odio laudantium cum dolore. Nihil, veritatis.</p>
+            </div>
+
+            <div class="box">
+                <img src="dentist.png" alt="">
+                <h3>Live Dental advisory</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sint quasi minus officia repellat fugit neque qui recusandae debitis saepe. Dignissimos ex fugiat vitae odio laudantium cum dolore. Nihil, veritatis.</p>
+            </div>
+
+            <div class="box">
+                <img src="image 6.png" alt="">
+                <h3>Cavity inspection</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sint quasi minus officia repellat fugit neque qui recusandae debitis saepe. Dignissimos ex fugiat vitae odio laudantium cum dolore. Nihil, veritatis.</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section endss -->
+    <!-- process section starts -->
+    
+    <section class="process">
+        <h1 class="heading">work process</h1>
+        <div class="box-container container">
+            <div class="box">
+                <img src="WhatsApp Image 2024-04-01 at 3.57.25 PM.jpeg" alt="">
+                <h3>Cosmetic Dentistry</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil amet nulla eaque corporis saepe enim numquam quam provident. Reprehenderit perspiciatis commodi ipsam exercitationem odit corporis inventore minus rem amet asperiores.</p>
+            </div>
+
+            <div class="box">
+                <img src="WhatsApp Image 2024-01-31 at 2.38.43 PM.jpeg" alt="">
+                <h3>Pediatric Dentistry</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil amet nulla eaque corporis saepe enim numquam quam provident. Reprehenderit perspiciatis commodi ipsam exercitationem odit corporis inventore minus rem amet asperiores.</p>
+            </div>
+
+            <div class="box">
+                <img src="types-of-dental-procedures-1080x675.jpeg" alt="">
+                <h3>Dental implants</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil amet nulla eaque corporis saepe enim numquam quam provident. Reprehenderit perspiciatis commodi ipsam exercitationem odit corporis inventore minus rem amet asperiores.</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- process section ends -->
+    <!-- reviews section starts -->
+
+    <section class="reviews" id="reviews">
+        <h1 class="heading">satisified clients</h1>
+        <div class="box-container container">
+            <div class="box">
+                <img src="a beautiful  happy c.jpg" alt="">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis praesentium maiores dolorem saepe. Accusantium obcaecati totam inventore consectetur, id libero? Quis praesentium nisi quia officiis alias fuga, nemo et!</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <h3>Willy x</h3>
+                <span>satisified client</span>
+            </div>
+
+            <div class="box">
+                <img src="a single passport pi(1).jpg" alt="">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis praesentium maiores dolorem saepe. Accusantium obcaecati totam inventore consectetur, id libero? Quis praesentium nisi quia officiis alias fuga, nemo et!</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <h3>Willy x</h3>
+                <span>satisified client</span>
+            </div>
+
+            <div class="box">
+                <img src="a beautiful  happy c.jpg" alt="">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis praesentium maiores dolorem saepe. Accusantium obcaecati totam inventore consectetur, id libero? Quis praesentium nisi quia officiis alias fuga, nemo et!</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <h3>Willy x</h3>
+                <span>satisified client</span>
+            </div>
+        </div>
+    </section>
+
+
+
+    <!-- reviews section endss -->
+    <!-- Contact section start -->
+     <section class="contact" id="contact">
+
+        <h1 class="heading"> Make appointment </h1>
+
+        <form action="<?php echo $SERVER['PHP_SELF']; ?>" method="post">
+            <span>Your Name: </span>
+            <input type="text" name="name" placeholder="Enter your name" class="box">
+            <span>Your Email: </span>
+            <input type="email" name="email" placeholder="Enter your email" class="box">
+            <span>Your Number: </span>
+            <input type="number" name="number" placeholder="Enter your number" class="box">
+            <span>Your Dentist: </span>
+            <!-- <input type="select" name="name" placeholder="Enter your Dentist" class="box"> -->
+            <select name="doctor" class="box" required>
+                <option value="">Select a Dentist</option>
+                <option value="Dr. John Doe">Dr. John Doe - Dentist</option>
+                <option value="Dr. Alice Smith">Dr. Alice Smith - Orthodontist</option>
+                <option value="Dr. Mark Wilson">Dr. Mark Wilson - Surgeon</option>
+            </select>
+            <span>appointment date: </span>
+            <input type="datetime-local" name="date"  class="box">
+            <input type="submit" value="make appointment" name="submit" class="link-btn">
+
+        </form>
+
+     </section>
+
+
+    <!-- Contact section ends -->
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- custom js file link -->
+      <script src="dentalcare.js"></script>
+</body>
+</html>
